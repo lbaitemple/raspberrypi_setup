94d4eb968aee33aeaa32bca6e8f56af63ca04b52
lbaitemple <lbai@temple.edu>
2019-08-13 16:24:25 -0400

    Update access.sh

--- startup/access.sh
+++ startup/access.sh
@@ -3,5 +3,5 @@
 #acc=`ifconfig  | grep wlan0 | cut -d':' -f 6-7 | sed s/://`
 acc=`ip addr show $(awk 'NR==3{print $1}' /proc/net/wireless | tr -d :) | awk '/ether/{print $2}' | cut -d':' -f 5-7 | sed s/:// `
 pi_acc=`echo Pi3-AC-${acc^^}`
-cmd=`/snap/bin/wifi-ap.config set wifi.ssid=${pi_acc}`
+cmd=`sudo /snap/bin/wifi-ap.config set wifi.ssid=${pi_acc}`
 echo "DONE"
